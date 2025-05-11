0f949f4d5e4c296673ac96b9df40922cd2960a80
Sergei Finogenov <serfinogenov@yandex.ru>
2025-05-12 02:21:36 +0300

    fixing the HTML-code in the templates 'home.html' and 'contacts.html' with correct links to each other

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from catalog.apps import CatalogConfig
+from catalog import views
 
-app_name = 'catalog'
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', views.home, name='home'),
