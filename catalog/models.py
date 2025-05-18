e1c32ceed53580b1284927fbe3ef3a535f510179
Sergei Finogenov <serfinogenov@yandex.ru>
2025-05-18 22:54:31 +0300

    creating the models 'Product' and 'Category'

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Наименование продукта",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to="photos/",
+        blank=True,
+        null=True,
+        verbose_name="Изображение продукта",
+        help_text="Загрузите изображение продукта",
+    )
+    category = models.CharField(
+        max_length=100, verbose_name="Категория", help_text="Введите категорию продукта"
+    )
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Наименование категории",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
