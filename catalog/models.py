8702609813887423c5ec5633d40e5351c437aed7
Sergei Finogenov <serfinogenov@yandex.ru>
2025-05-18 23:15:41 +0300

    adding the ForeignKey

--- catalog/models.py
+++ catalog/models.py
@@ -1,40 +1,6 @@
 from django.db import models
 
 
-class Product(models.Model):
-    title = models.CharField(
-        max_length=100,
-        verbose_name="Наименование продукта",
-        help_text="Введите наименование продукта",
-    )
-    description = models.TextField(
-        verbose_name="Описание продукта",
-        help_text="Введите описание продукта",
-        blank=True,
-        null=True,
-    )
-    image = models.ImageField(
-        upload_to="photos/",
-        blank=True,
-        null=True,
-        verbose_name="Изображение продукта",
-        help_text="Загрузите изображение продукта",
-    )
-    category = models.CharField(
-        max_length=100, verbose_name="Категория", help_text="Введите категорию продукта"
-    )
-    price = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = "Продукт"
-        verbose_name_plural = "Продукты"
-
-
 class Category(models.Model):
     title = models.CharField(
         max_length=100,
@@ -54,3 +20,34 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+class Product(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Наименование продукта",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to="photos/",
+        blank=True,
+        null=True,
+        verbose_name="Изображение продукта",
+        help_text="Загрузите изображение продукта",
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
