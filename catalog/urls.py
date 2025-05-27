a07160472cc07f482b03748c02c79728c11fe7d9
Sergei Finogenov <serfinogenov@yandex.ru>
2025-05-27 23:48:04 +0300

    adding the view for a single product 'single_product' to urlpatterns

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,6 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts')
+    path('contacts/', views.contacts, name='contacts'),
+    path('single_product/<int:pk>', views.single_product, name='single_product'),
 ]
