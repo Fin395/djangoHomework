ed0915328516bc55e3ce79459cb0bd33677907c9
Sergei Finogenov <serfinogenov@yandex.ru>
2025-05-28 00:07:46 +0300

    adding the view 'main_page' to the urlpatterns

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
     path('single_product/<int:pk>', views.single_product, name='single_product'),
+    path('main_page', views.main_page, name='main_page'),
+
 ]
