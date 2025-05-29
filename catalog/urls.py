4b3e66f22243b1a8109c4ba7c1608d24bb55de71
Sergei Finogenov <serfinogenov@yandex.ru>
2025-05-29 03:12:14 +0300

    updating the templates 'main_page' and 'contacts'

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('contacts/', views.contacts, name='contacts'),
-    path('single_product/<int:pk>', views.single_product, name='single_product'),
+    path('single_product/<int:pk>/', views.single_product, name='single_product'),
     path('main_page/', views.main_page, name='main_page'),
 
 ]
