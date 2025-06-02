c9212658200fa8cca1abf2d79d1508649384e76b
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-02 21:53:39 +0300

    replacing FBV for CBV for the 'Catalog' view

--- catalog/views.py
+++ catalog/views.py
@@ -1,39 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Product
 from catalog.models import Product
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        return HttpResponse(f"Спасибо, {name}! Ваши данные успешно отправлены.")
-    return render(request, 'catalog/contacts.html')
+class ProductsListView(ListView):
+    model = Product
 
 
-def single_product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, 'catalog/product.html', context)
+class ProductDetailView(DetailView):
+    model = Product
 
 
-def main_page(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'catalog/main_page.html', context)
-
-
-def create_product(request):
-    if request.method == 'POST':
-        name = request.POST.get('Наименование продукта')
-        description = request.POST.get('Описание продукта')
-        image = request.POST.get('Изображение продукта')
-        category = request.POST.get('Категория продукта')
-        price = request.POST.get('Цена продукта')
-        created_at = request.POST.get('Дата создания продукта')
-        updated_at = request.POST.get('Дата последнего изменения')
-        Product.objects.create(name=name, description=description, image=image, category=category, price=price, created_at=created_at, updated_at=updated_at)
-        return HttpResponse("Спасибо! Товар успешно создан!")
-    return render(request, 'catalog/create_product.html')
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
