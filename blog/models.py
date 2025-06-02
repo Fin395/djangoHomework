48ec9da18fe5e89d47dbd296daf18c4f6a7faa05
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-02 22:31:59 +0300

    creating the model 'MyBlog'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class MyBlog(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Заголовок",
+        help_text="Введите заголовок блога",
+    )
+    content = models.TextField(
+        verbose_name="Содержимое блога",
+        help_text="Введите содержимое блога",
+        blank=True,
+        null=True,
+    )
+    preview = models.ImageField(
+        upload_to="previews/",
+        blank=True,
+        null=True,
+        verbose_name="Превью блога",
+        help_text="Загрузите изображение",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+    views = models.IntegerField()
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Блог"
+        verbose_name_plural = "Блоги"
