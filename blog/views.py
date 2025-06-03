3e890657933bd4e6c4aec1bc90a1977914b975fb
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-04 02:15:11 +0300

    redefinition of methods

--- blog/views.py
+++ blog/views.py
@@ -3,22 +3,37 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from blog.models import Article
 
+
 class BlogView(TemplateView):
     template_name = 'blog/main_page.html'
+
 
 class BlogContactsView(TemplateView):
     template_name = 'blog/contacts.html'
 
+
 class ArticleDetailView(DetailView):
     model = Article
 
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.views += 1
+        obj.save()
+        return obj
+
+
 class ArticlesListView(ListView):
     model = Article
+
+    def get_queryset(self):
+        return Article.objects.filter(is_published=True)
+
 
 class ArticleCreateView(CreateView):
     model = Article
     fields = ['title', 'content', 'preview']
     success_url = reverse_lazy('blog:articles')
+
 
 class ArticleUpdateView(UpdateView):
     model = Article
@@ -26,6 +41,7 @@ class ArticleUpdateView(UpdateView):
 
     def get_success_url(self, **kwargs):
         return reverse_lazy('blog:article', kwargs={'pk': self.object.pk})
+
 
 class ArticleDeleteView(DeleteView):
     model = Article
