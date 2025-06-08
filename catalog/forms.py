f173cb7bfff7ceea0bc87357da8626e0453f7771
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-08 19:38:35 +0300

    creating validation for name and description of Product

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from .models import Product
+from django.core.exceptions import ValidationError
+
+
+FORBIDDEN_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -32,3 +36,15 @@ class ProductForm(forms.ModelForm):
             'class': 'form-control',  # Добавление CSS-класса для стилизации поля
             'placeholder': 'Укажите цену'  # Текст подсказки внутри поля
         })
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        for word in FORBIDDEN_WORDS:
+            if word.lower() in name:
+                raise ValidationError(f"Слово {word} не может содержаться в наименовании продукта")
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        for word in FORBIDDEN_WORDS:
+            if word.lower() in description:
+                raise ValidationError(f"Слово {word} не может содержаться в описании продукта")
