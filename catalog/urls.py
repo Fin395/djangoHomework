ad2380a65add7333b96be940510b780ac08931b1
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-08 17:02:53 +0300

    adding the styling, CRUD using the form; creating template 'product_confirm_delete'

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductsListView, ProductDetailView, ContactsView, ProductCreateView
+from catalog.views import ProductsListView, ProductDetailView, ContactsView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 app_name = CatalogConfig.name
 
@@ -9,5 +10,7 @@ urlpatterns = [
     path('single_product/<int:pk>/', ProductDetailView.as_view(), name='single_product'),
     path('main_page/', ProductsListView.as_view(), name='main_page'),
     path('create/', ProductCreateView.as_view(), name='product_create'),
+    path('<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import ProductForm
 from .models import Product
@@ -20,6 +20,20 @@ class ProductsListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:main_page')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:main_page')
 
 
 class ContactsView(TemplateView):
