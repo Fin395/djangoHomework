af20b4dc04506a504a67825c4674eb9b9db252a7
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-16 17:20:12 +0300

    including users urls into config urls

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from users.apps import UsersConfig
+from blog.views import BlogView, BlogContactsView, ArticleDetailView, ArticlesListView, ArticleCreateView, \
+    ArticleUpdateView, ArticleDeleteView
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('main/', BlogView.as_view(), name='article_main'),
+    path('contacts/', BlogContactsView.as_view(), name='article_contacts'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('', ArticlesListView.as_view(), name='article_list'),
+    path('create/', ArticleCreateView.as_view(), name='article_create'),
+    path('<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+
+]
