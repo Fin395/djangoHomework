2bee4ec2c79ec4f9182dfebe5a6a2db7c747947a
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-16 17:24:11 +0300

    fixing the urls

--- users/urls.py
+++ users/urls.py
@@ -7,11 +7,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('main/', BlogView.as_view(), name='article_main'),
-    path('contacts/', BlogContactsView.as_view(), name='article_contacts'),
-    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
-    path('', ArticlesListView.as_view(), name='article_list'),
-    path('create/', ArticleCreateView.as_view(), name='article_create'),
-    path('<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
-    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+
 
 ]
