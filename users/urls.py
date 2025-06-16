25213eecbd2f41e7b9fc6e9e3e0d872ec01947a7
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-16 18:22:53 +0300

    preliminary created templates, views and urls for user login and registration

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from users.apps import UsersConfig
+from django.contrib.auth.views import LoginView, LogoutView
 from blog.views import BlogView, BlogContactsView, ArticleDetailView, ArticlesListView, ArticleCreateView, \
     ArticleUpdateView, ArticleDeleteView
+from users.views import RegisterView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('main/', BlogView.as_view(), name='article_main'),
-
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import CustomUserRegistrationForm
 
-# Create your views here.
+class RegisterView(CreateView):
+    form_class = CustomUserRegistrationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('products:product_list')
