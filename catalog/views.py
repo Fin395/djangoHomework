51c4e419b3eb033bde464f388b27bc28b9993367
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-17 15:35:52 +0300

    updating 'catalog/views.py' with restriction of the access for unauthenticated users

--- catalog/views.py
+++ catalog/views.py
@@ -1,38 +1,37 @@
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .forms import ProductForm
 from .models import Product
 
-
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:product_list')
-
+    login_url = reverse_lazy('users:login')
 
 class ProductsListView(ListView):
     model = Product
 
-
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
+    login_url = reverse_lazy('users:login')
 
-
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
+    login_url = reverse_lazy('users:login')
 
     def get_success_url(self, **kwargs):
         return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
 
-
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:product_list')
-
+    login_url = reverse_lazy('users:login')
 
 class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
