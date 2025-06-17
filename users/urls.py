abea100731055bbf18f46648e74d6b3747f16eee
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-17 15:11:06 +0300

    updating registration view with sending mails

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from users.apps import UsersConfig
 from django.contrib.auth.views import LoginView, LogoutView
-from blog.views import BlogView, BlogContactsView, ArticleDetailView, ArticlesListView, ArticleCreateView, \
-    ArticleUpdateView, ArticleDeleteView
-from users.views import RegisterView
+from users.views import UserRegisterView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('register/', RegisterView.as_view(), name='register'),
-
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(next_page='catalog:product_list'), name='logout'),
 ]
