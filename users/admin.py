2e962b7cc2c5cce8285b0a33cdcdf87a3c621b57
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-21 18:34:55 +0300

    fixing the code in 'users/admin.py' to view the users

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 # from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email')
